ede11ec655104acc8e0777a36b13bcebc4b24747
petlyh <88139840+petlyh@users.noreply.github.com>
2023-09-07 14:39:03 +0200

    Add test for furigana parsing

---
+++ test/furigana_test.dart
@@ -0,0 +1,18 @@
+import "package:flutter_test/flutter_test.dart";
+import "package:jsdict/models/models.dart";
+import "package:jsdict/packages/jisho_client/jisho_client.dart";
+
+Future<void> Function() _furiganaTest(String wordId, String text, String reading) {
+  return () async {
+    final wordDetails = await JishoClient().wordDetails(wordId);
+    expect(wordDetails.word.getText(), text);
+    expect(wordDetails.word.getReading(), reading);
+  };
+}
+
+void main() {
+  test("nani", _furiganaTest("何", "何", "なに"));
+  test("arifureta", _furiganaTest("有り触れた", "有り触れた", "ありふれた"));
+  test("gohan", _furiganaTest("ご飯", "ご飯", "ごはん"));
+  test("gozonji", _furiganaTest("ご存じ", "ご存知", "ごぞんじ"));
+}
