f589938c4d24d59729ce48ed0308c9393d03bb19
petlyh <88139840+petlyh@users.noreply.github.com>
2023-11-07 22:02:34 +0100

    Partially load Name kanji

--- lib/screens/name_details_screen.dart
+++ lib/screens/name_details_screen.dart
@@ -26,51 +26,47 @@ class NameDetailsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Name")),
-      body: LoaderWidget(
-          onLoad: _getKanji,
-          handler: (kanjiList) {
-            return SingleChildScrollView(
-              child: Container(
-                margin: const EdgeInsets.all(8.0),
-                child: Column(
-                  children: [
-                    Container(
-                        alignment: Alignment.center,
-                        padding: const EdgeInsets.all(12),
-                        child: Column(
-                          children: [
-                            Text(
-                                name.japanese +
-                                    (name.reading != null
-                                        ? "\n${name.reading}"
-                                        : ""),
-                                style: const TextStyle(fontSize: 20).jp(),
-                                textAlign: TextAlign.center),
-                            const SizedBox(height: 12),
-                            Text(name.english,
-                                style: const TextStyle(fontSize: 18),
-                                textAlign: TextAlign.center),
-                          ],
-                        )),
-                    InfoChip(name.type),
-                    const SizedBox(height: 12),
-                    if (name.wikipediaWord != null)
-                      ElevatedButton(
-                          onPressed: pushScreen(context,
-                              WikipediaScreen.fromWord(name.wikipediaWord!)),
-                          child: const Text("Wikipedia")),
-                    const SizedBox(height: 16),
-                    ListView.builder(
-                        physics: const NeverScrollableScrollPhysics(),
-                        shrinkWrap: true,
-                        itemCount: kanjiList.length,
-                        itemBuilder: (_, index) =>
-                            KanjiItem(kanji: kanjiList[index]))
-                  ],
-                ),
-              ),
-            );
-          }),
+      body: SingleChildScrollView(
+        child: Container(
+          margin: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Container(
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.all(12),
+                  child: Column(
+                    children: [
+                      Text(
+                          name.japanese +
+                              (name.reading != null ? "\n${name.reading}" : ""),
+                          style: const TextStyle(fontSize: 20).jp(),
+                          textAlign: TextAlign.center),
+                      const SizedBox(height: 12),
+                      Text(name.english,
+                          style: const TextStyle(fontSize: 18),
+                          textAlign: TextAlign.center),
+                    ],
+                  )),
+              InfoChip(name.type),
+              const SizedBox(height: 12),
+              if (name.wikipediaWord != null)
+                ElevatedButton(
+                    onPressed: pushScreen(
+                        context, WikipediaScreen.fromWord(name.wikipediaWord!)),
+                    child: const Text("Wikipedia")),
+              const SizedBox(height: 16),
+              LoaderWidget(
+                  onLoad: _getKanji,
+                  handler: (kanjiList) => ListView.builder(
+                      physics: const NeverScrollableScrollPhysics(),
+                      shrinkWrap: true,
+                      itemCount: kanjiList.length,
+                      itemBuilder: (_, index) =>
+                          KanjiItem(kanji: kanjiList[index]))),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
