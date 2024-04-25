145e65a05f077553f1ec5c4eddb9730d9973e051
petlyh <88139840+petlyh@users.noreply.github.com>
2024-04-25 10:54:48 +0200

    Refactor list extensions

--- lib/packages/list_extensions.dart
+++ lib/packages/list_extensions.dart
@@ -1,35 +1,21 @@
+import "package:collection/collection.dart";
+
 extension ListExtensions on List<dynamic> {
-  List<T> intersperce<T>(T item) {
-    final typedList = List<T>.from(this);
-
-    if (typedList.length < 2) {
-      return typedList;
-    }
-
-    final insertCount = typedList.length - 1;
-
-    for (var i = 0; i < insertCount; i++) {
-      typedList.insert(1 + i * 2, item);
-    }
-
-    return typedList;
-  }
+  List<T> intersperce<T>(T item) => List<T>.from(this)
+      .map((element) => [item, element])
+      .flattened
+      .toList()
+      .sublist(1);
 
   List<T> deduplicate<T>() => toSet().toList() as List<T>;
 
-  List<T> deduplicateBy<T>(T Function(T item) transform) {
-    final List<T> result = [];
-
-    for (final item in this as List<T>) {
-      if (result.map(transform).contains(transform(item))) {
-        continue;
-      }
-
-      result.add(item);
-    }
-
-    return result;
-  }
+  List<T> deduplicateBy<T>(T Function(T item) transform) =>
+      (this as List<T>).fold(
+        [],
+        (list, item) => list.map(transform).contains(transform(item))
+            ? list
+            : list + [item],
+      );
 }
 
 extension StringListExtensions on List<String> {
