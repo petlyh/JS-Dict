38f0ecd87215eb30e3da1e2162fb414f93d48cd4
petlyh <88139840+petlyh@users.noreply.github.com>
2024-04-26 19:37:43 +0200

    Refactor isKanji helpers

--- lib/packages/is_kanji.dart
+++ lib/packages/is_kanji.dart
@@ -1,25 +1,18 @@
 import "package:collection/collection.dart";
 
+const _cjkUnifiedIdeographsStart = 0x4E00;
+const _cjkUnifiedIdeographsEnd = 0x9FFF;
+
 /// checks whether [text] only contains kanji characters.
-bool isKanji(String text) {
-  const cjkUnifiedIdeographsStart = 0x4E00;
-  const cjkUnifiedIdeographsEnd = 0x9FFF;
-
-  final codeUnits = text.trim().codeUnits;
-  final nonKanji = codeUnits.firstWhereOrNull((unit) =>
-      !(cjkUnifiedIdeographsStart <= unit && unit <= cjkUnifiedIdeographsEnd));
-
-  return nonKanji == null;
-}
+bool isKanji(String text) =>
+    text.trim().codeUnits.firstWhereOrNull((unit) =>
+        !(_cjkUnifiedIdeographsStart <= unit &&
+            unit <= _cjkUnifiedIdeographsEnd)) ==
+    null;
 
 /// checks whether [text] doesn't contain any kanji characters.
-bool isNonKanji(String text) {
-  const cjkUnifiedIdeographsStart = 0x4E00;
-  const cjkUnifiedIdeographsEnd = 0x9FFF;
-
-  final codeUnits = text.trim().codeUnits;
-  final firstKanji = codeUnits.firstWhereOrNull((unit) =>
-      cjkUnifiedIdeographsStart <= unit && unit <= cjkUnifiedIdeographsEnd);
-
-  return firstKanji == null;
-}
+bool isNonKanji(String text) =>
+    text.trim().codeUnits.firstWhereOrNull((unit) =>
+        _cjkUnifiedIdeographsStart <= unit &&
+        unit <= _cjkUnifiedIdeographsEnd) ==
+    null;
