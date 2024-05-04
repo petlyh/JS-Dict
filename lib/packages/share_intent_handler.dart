fe081f1b5888422f7fa2e0f40fa161b6734c27da
petlyh <88139840+petlyh@users.noreply.github.com>
2024-05-04 15:10:23 +0200

    Small refactor in ShareIntentHandler

--- lib/packages/share_intent_handler.dart
+++ lib/packages/share_intent_handler.dart
@@ -1,5 +1,6 @@
 import "dart:async";
 
+import "package:collection/collection.dart";
 import "package:flutter/material.dart";
 import "package:jsdict/packages/navigation.dart";
 import "package:jsdict/packages/remove_tags.dart";
@@ -44,7 +45,7 @@ class ShareIntentHandler {
     }
 
     final textFile =
-        files.where((file) => file.mimeType == "text/plain").firstOrNull;
+        files.firstWhereOrNull((file) => file.mimeType == "text/plain");
 
     if (textFile == null) {
       return;
