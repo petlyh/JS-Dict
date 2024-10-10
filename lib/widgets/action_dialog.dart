ade69e3bedbb38e31bfa287f18e52c2dca3253a2
petlyh <88139840+petlyh@users.noreply.github.com>
2024-10-10 21:51:41 +0200

    Refactor showActionDialog

--- lib/widgets/action_dialog.dart
+++ lib/widgets/action_dialog.dart
@@ -6,29 +6,19 @@ import "package:url_launcher/url_launcher.dart";
 void showActionDialog(BuildContext context, List<ActionTile> actions) =>
     showModalBottomSheet(
       context: context,
-      builder: (_) => ActionDialog(actions: actions),
+      builder: (_) => Padding(
+        padding: const EdgeInsets.only(bottom: 8),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: actions,
+        ),
+      ),
       showDragHandle: true,
     );
 
-class ActionDialog extends StatelessWidget {
-  const ActionDialog({super.key, required this.actions});
-
-  final List<ActionTile> actions;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 8),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: actions,
-      ),
-    );
-  }
-}
-
 class ActionTile extends StatelessWidget {
   const ActionTile.text(this.name, this.data, {super.key}) : isURL = false;
+
   const ActionTile.url(this.data, {super.key})
       : isURL = true,
         name = "Link";
@@ -37,23 +27,19 @@ class ActionTile extends StatelessWidget {
   final String data;
   final bool isURL;
 
-  void onCopy(BuildContext context) =>
+  void _onCopy(BuildContext context) =>
       copyText(context, data, name: name.toLowerCase());
 
-  void onShare(BuildContext context) =>
+  void _onShare(BuildContext context) =>
       isURL ? Share.shareUri(Uri.parse(data)) : Share.share(data);
 
-  void onOpenBrowser(BuildContext context) =>
+  void _onOpenBrowser(BuildContext context) =>
       launchUrl(Uri.parse(data), mode: LaunchMode.externalApplication);
 
-  void Function() pop(
-    BuildContext context,
-    void Function(BuildContext context) callback,
-  ) =>
-      () {
-        Navigator.of(context).pop();
-        callback(context);
-      };
+  void _pop(BuildContext context, void Function(BuildContext) callback) {
+    Navigator.of(context).pop();
+    callback(context);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -69,17 +55,17 @@ class ActionTile extends StatelessWidget {
           if (isURL)
             IconButton(
               tooltip: "Open in Browser",
-              onPressed: pop(context, onOpenBrowser),
+              onPressed: () => _pop(context, _onOpenBrowser),
               icon: const Icon(Icons.open_in_browser),
             ),
           IconButton(
             tooltip: "Copy",
-            onPressed: pop(context, onCopy),
+            onPressed: () => _pop(context, _onCopy),
             icon: const Icon(Icons.copy),
           ),
           IconButton(
             tooltip: "Share",
-            onPressed: pop(context, onShare),
+            onPressed: () => _pop(context, _onShare),
             icon: const Icon(Icons.share),
           ),
         ],
