28f80dd5b06d917bd1acdabd58c868d5858d9b5a
petlyh <88139840+petlyh@users.noreply.github.com>
2024-11-05 18:29:33 +0100

    Refactor isKanjiOnly and isNonKanji

--- lib/packages/is_kanji.dart
+++ lib/packages/is_kanji.dart
@@ -1,21 +1,23 @@
-import "package:collection/collection.dart";
-
 const _cjkUnifiedIdeographsStart = 0x4E00;
 const _cjkUnifiedIdeographsEnd = 0x9FFF;
 
 /// checks whether [text] only contains kanji characters.¨
-bool isKanjiOnly(String text) =>
-    text.trim().codeUnits.firstWhereOrNull(
-          (unit) => !(_cjkUnifiedIdeographsStart <= unit &&
-              unit <= _cjkUnifiedIdeographsEnd),
-        ) ==
-    null;
+bool isKanjiOnly(String text) => text
+    .trim()
+    .codeUnits
+    .where(
+      (unit) => !(_cjkUnifiedIdeographsStart <= unit &&
+          unit <= _cjkUnifiedIdeographsEnd),
+    )
+    .isEmpty;
 
 /// checks whether [text] doesn't contain any kanji characters.
-bool isNonKanji(String text) =>
-    text.trim().codeUnits.firstWhereOrNull(
-          (unit) =>
-              _cjkUnifiedIdeographsStart <= unit &&
-              unit <= _cjkUnifiedIdeographsEnd,
-        ) ==
-    null;
+bool isNonKanji(String text) => text
+    .trim()
+    .codeUnits
+    .where(
+      (unit) =>
+          _cjkUnifiedIdeographsStart <= unit &&
+          unit <= _cjkUnifiedIdeographsEnd,
+    )
+    .isEmpty;
