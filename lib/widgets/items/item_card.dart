a7f664e52b115f8a892990ed6d2319ddda0952d0
petlyh <88139840+petlyh@users.noreply.github.com>
2024-11-06 20:30:34 +0100

    Refactor ItemCard widget

--- lib/widgets/items/item_card.dart
+++ lib/widgets/items/item_card.dart
@@ -4,21 +4,21 @@ class ItemCard extends StatelessWidget {
   const ItemCard({
     super.key,
     required this.child,
-    this.onTap,
-    this.onLongPress,
+    required this.onTap,
+    required this.onLongPress,
   });
 
   final Widget child;
-  final Function()? onTap;
-  final Function()? onLongPress;
+  final void Function() onTap;
+  final void Function() onLongPress;
 
   @override
   Widget build(BuildContext context) {
     return Card(
       child: InkWell(
         borderRadius: BorderRadius.circular(12),
-        onTap: onTap ?? () => {},
-        onLongPress: onLongPress ?? () => {},
+        onTap: onTap,
+        onLongPress: onLongPress,
         child: child,
       ),
     );
